683160ddafd4ec79bcae8e5b4d4a607f9bbc7600
enzoribas <enzortorres2005@gmail.com>
2024-05-23 19:48:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# html-css
+ Estudos de HTML5 e CSS3
---
+++ exercicios/shorthand/agualimpa.py
@@ -0,0 +1,6 @@
+# situacao = str(input("A água está limpa? "))
+# if situacao == 'sim':
+#     print("Então pode ingerir água e tomar seu banhinho!")
+# else:
+#     print("Não pode ingerir, tomar banho, lavar louça, resumindo, tocar na água!")
+print("Hello world")
